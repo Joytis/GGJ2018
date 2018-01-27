38a67a2139e77c3eaf34400f82df16cdf719abb1
Christopher George <SoccerGeorge10@gmail.com>
2018-01-27 14:03:02 -0600

    Added initial snatch and grab

--- Assets/Scripts/Movement/ReflectorMovement.cs
+++ Assets/Scripts/Movement/ReflectorMovement.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReflectorMovement : MonoBehaviour {
+public class ReflectorMovement : MonoBehaviour
+{
+    GameObject _spaceCraft;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    #region MonoBehaviour
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (_spaceCraft != null) {
+            transform.position = _spaceCraft.transform.position + (Vector3.back * 50f);
+        }
+    }
+    #endregion
+
+    #region Public Methods
+    public void SetSpaceCraft (GameObject go) {
+        _spaceCraft = go;
+    }
+
+    public void DestroySelf () {
+        Destroy(this);
+        _spaceCraft = null;
+    }
+    #endregion
 }
