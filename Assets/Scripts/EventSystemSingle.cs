f195915813905b68e86076e2ebe73e0b0a227130
Clark Chambers <clarkchambers94@gmail.com>
2018-01-28 12:34:22 -0600

    Play Again works.

---
+++ Assets/Scripts/EventSystemSingle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSystemSingle : MonoBehaviour {
+
+
+    public static EventSystemSingle Instance;
+
+    void Awake () {
+        if (Instance == null) {
+            Instance = this;
+        }
+
+        else {
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
