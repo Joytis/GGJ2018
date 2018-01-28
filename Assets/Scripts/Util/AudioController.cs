7d4233d6ce1aae4af33563875aa90a677fd5cfac
Christopher George <SoccerGeorge10@gmail.com>
2018-01-27 23:00:30 -0600

    Added AudioController prefab and made it an instance

--- Assets/Scripts/Util/AudioController.cs
+++ Assets/Scripts/Util/AudioController.cs
@@ -5,24 +5,20 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class AudioController : MonoBehaviour
 {
-    //public static AudioController Instance;
+    public static AudioController Instance;
 
-    public AudioClip[] clips;
+    public AudioClip[] Clips;
     AudioSource _audio;
 
     void Awake () {
-        //if (Instance == null) {
-        //    Instance = this;
-        //}
-        //Instance._audio = GetComponent<AudioSource>();
-
-        _audio = GetComponent<AudioSource>();
+        if (Instance == null) {
+            Instance = this;
+        }
+        Instance._audio = GetComponent<AudioSource>();
     }
 
     public void PlayAudio (int index) {
-       //Instance._audio.clip = clips[index];
-       //Instance._audio.Play();
-        _audio.clip = clips[index];
-        _audio.Play();
+        Instance._audio.clip = Clips[index];
+        Instance._audio.Play();
     }
 }
