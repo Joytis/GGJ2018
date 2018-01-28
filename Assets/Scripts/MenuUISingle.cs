399c262b6d4204ed3c2e25d6965839112faf8090
ncompton13 <ncompton13@aol.com>
2018-01-28 13:23:38 -0600

    Added stuff again

---
+++ Assets/Scripts/MenuUISingle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUISingle : MonoBehaviour {
+
+
+    public static MenuUISingle Instance;
+
+    void Awake () {
+        if (Instance == null) {
+            Instance = this;
+        }
+
+        else {
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
