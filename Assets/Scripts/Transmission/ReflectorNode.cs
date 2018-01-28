2ba6e475a45ff492270a1a9f030c0e6c3f7fa3f8
Clark Chambers <clarkchambers94@gmail.com>
2018-01-28 11:35:45 -0600

    More sound effect shit. Going to duplicate rock prefabs for testystuff

--- Assets/Scripts/Transmission/ReflectorNode.cs
+++ Assets/Scripts/Transmission/ReflectorNode.cs
@@ -6,12 +6,14 @@ public class ReflectorNode : MonoBehaviour
 {
     bool _canPickUp = false;
     Vector3 _centerOffset = new Vector3(5f, 13.4f, 5f);
+    public int indexForPickupSound;
 
     void Update () {
         if (_canPickUp) {
             if (Input.GetKeyDown(KeyCode.Space) && Vector3.Distance(SpaceCraftManager.Instance.GetSpaceCraft.transform.position, transform.position + _centerOffset) < 45f) {
                 gameObject.AddComponent<ReflectorMovement>();
                 SpaceCraftManager.Instance.GetPlayerMovement.SetReflector(gameObject);
+                if (AudioController.Instance != null) { AudioController.Instance.PlayAudio(indexForPickupSound); }
             }
         }
     }
